bbaf1d5660741aa0628a494235e531b75abd8775
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-10-20 17:44:47 +0300

    Logger Service Created

---
+++ AwsCloudWatch/WebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Services;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly WeatherService _weatherService;
+
+        public WeatherForecastController(WeatherService weatherService)
+        {
+            _weatherService = weatherService;
+        }
+
+        [HttpGet("weather/{city}")]
+        public async Task<IActionResult> GetForCity([FromRoute] string city, [FromQuery] int days = 5)
+        {
+            return Ok(await _weatherService.GetWeatherForCityAsync(city, days));
+        }
+    }
+}
---
+++ AwsCloudWatch/WebAPI/Services/LoggingServiceRegistration.cs
@@ -0,0 +1,27 @@
+﻿using Amazon.CloudWatchLogs;
+using Serilog;
+using Serilog.Sinks.AwsCloudWatch;
+using ILogger = Serilog.ILogger;
+
+namespace WebAPI.Services;
+
+public static class LoggingServiceRegistration
+{
+    public static IServiceCollection AddLoggingServices(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        ILogger logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.AmazonCloudWatch(
+                logGroup: $"{builder.Environment.EnvironmentName}/{builder.Environment.ApplicationName}",
+                logStreamPrefix: DateTime.UtcNow.ToString("yyyyMMddHHmmssfff"),
+                cloudWatchClient: new AmazonCloudWatchLogsClient()
+            )
+            .CreateLogger();
+
+        builder.Logging.AddSerilog(logger);
+        services.AddSingleton(logger);
+
+        return services;
+    }
+}
+
